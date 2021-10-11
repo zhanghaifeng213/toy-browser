368196ebc41a0460ef24e60a6c1bded9183b17ff
zhf <zhanghf_123@sina.com>
2021-10-11 21:31:22 +0800

    第一步 遇到style标签时，我们把css规则保存起来 这里我们调用CSS Parser来分析CSS规则 这里我们必须要仔细研究此库分析CSS规则的格式

--- parser.js
+++ parser.js
@@ -1,7 +1,16 @@
+const css = require("css");
+
 let currentToken = null;
 let currentAttribute = null;
 let currentTextNode = null;
 let stack = [{type:"document", children:[]}]
+// 加入一个新的函数，addCSSRules,这里我们把CSS规则暂存到一个数组里
+let rules = [];
+function addCSSRules(text) {
+  var ast = css.parse(text)
+  console.log(JSON.stringify(ast, null, "    "))
+  rules.push(...ast.stylesheet.rules)
+}
 
 function emit(token) {
   // if(token.type=="text") {
@@ -36,6 +45,10 @@ function emit(token) {
     if (top.tagName !== token.tagName) {
       throw new Error("Tag start end doesn't match!");
     } else {
+      // +++++++++++遇到style标签时，执行添加css规则的操作+++++++++++
+      if(top.tagName === "style"){
+        addCSSRules(top.children[0].content)
+      }
       stack.pop();
     }
     currentTextNode = null;
